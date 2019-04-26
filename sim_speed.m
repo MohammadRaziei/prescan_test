e25560da8ee0ce40e8f7c62b8140c2f573ec94a3
mohammadraziei <mohammadraziei1375@gmail.com>
2019-04-26 22:32:51 +0430

    matlab files changed

--- sim_speed.m
+++ sim_speed.m
@@ -1,3 +1,11 @@
-speed = 0.10;
+% speed = 0.10;
 
 set_param('Experiment_3_cs/Audi_A8_Sedan_1/InitialVelocity','Value','speed')
+set_param('Experiment_3_cs','SimulationCommand','update')
+
+set_param('Experiment_3_cs','SimulationCommand','pause')
+set_param('Experiment_3_cs','SimulationCommand','continue')
+set_param('Experiment_3_cs','SimulationCommand','stop')
+set_param('Experiment_3_cs','SimulationCommand','start')
+
+
--- startup.m
+++ startup.m
@@ -1,3 +1,11 @@
 if ~matlab.engine.isEngineShared
     matlab.engine.shareEngine('MATLAB_PRESCAN_engine')
 end
+speed = 0.1;
+% parfor i = 1:2  
+%     if i == 1
+%         sim('Experiment_3_cs',inf)
+%     end
+% end
+%         
+%         
